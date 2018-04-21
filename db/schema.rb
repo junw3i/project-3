055028edd6dcf1e2c6f829790f242e91e5d284e0
junw3i <cj@Juns-MacBook.local>
2018-04-21 11:36:52 +0800

    devise not fully working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_20_081856) do
+ActiveRecord::Schema.define(version: 2018_04_21_020312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2018_04_20_081856) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "role", default: "patient"
+    t.string "id_number"
+    t.string "gender"
+    t.integer "mobile"
+    t.boolean "is_live", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
