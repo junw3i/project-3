6f72ab87016f6e9199d57eb12c973abdd4e61674
1ung <leewhe@hotmail.com>
2018-04-23 16:51:21 +0800

    registration / login / forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        added_attrs = [:id_number, :gender, :mobile, :email, :password, :password_confirmation, :remember_me]
+        added_attrs = [:id_number, :gender, :mobile, :email, :first_name, :last_name, :password, :password_confirmation, :remember_me]
         devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
         devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     end
--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -1,4 +1,5 @@
 class PatientController < ApplicationController
   def index
   end
+
 end
