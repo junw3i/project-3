aa1733c2b47778d4d86a130fe2f5b8ec83bfe917
junw3i <cj@Juns-MacBook.local>
2018-04-26 10:13:24 +0800

    bug fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
     @queue_size = Request.all.size
     @first_in_line = Request.all.first
 
-    # view will depends on role    
+    # view will depends on role
     if current_user.nil?
        redirect_to new_user_session_path
     else
